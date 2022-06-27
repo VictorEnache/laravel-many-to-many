322fe1d46542a4c973a3bbd55633f5b80342c829
VictorEnache <victor22enache@gmail.com>
2022-06-27 23:18:31 +0200

    add title placeholder

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('page_title', 'Laravel Boolpress')</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -33,8 +33,8 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li><a class="nav-link {{Request::is('/') ? 'active' : ''}}" href="/">Starting Page</a></li>
-                        <li><a class="nav-link {{Request::is('info') ? 'active' : ''}}" href="/info">Info</a></li>
+                        <li><a class="nav-link {{Request::is('/') ? 'active bg-info' : ''}}" href="/">Starting Page</a></li>
+                        <li><a class="nav-link {{Request::is('info') ? 'active bg-info' : ''}}" href="/info">Info</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
