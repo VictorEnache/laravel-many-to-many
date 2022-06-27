197d04555c246328173268f6d5c8e4c3c3a66a50
VictorEnache <victor22enache@gmail.com>
2022-06-27 23:05:24 +0200

    initial commit and basic layout change

---
+++ resources/views/info.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Info</div>
+
+                <div class="card-body">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ex asperiores ipsam animi repellat culpa, enim quae laudantium architecto maiores atque deserunt fuga. Delectus qui doloremque, recusandae maiores neque culpa.
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/starting_page.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Starting Page</div>
+
+                <div class="card-body">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed inventore iusto, molestias fugit cumque saepe, quo dolores aperiam beatae ex perspiciatis suscipit necessitatibus ducimus velit porro! Odit tempora odio culpa!
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
