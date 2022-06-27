02e2f9f10d562a4cad1911714c2fd2c410648d7c
VictorEnache <victor22enache@gmail.com>
2022-06-28 01:04:24 +0200

    add individual error validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,7 +79,7 @@
             @if($errors->any())
             <div class="container">
                 <div class="alert alert-danger" role="alert">
-                    <ul>
+                    <ul class="mb-0">
                         @foreach($errors->all() as $error)
                         <li>
                             {{$error}}
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,16 +11,22 @@
                             @csrf
                             <div class="form-group">
                                 <label for="title">Title</label>
-                                <input type="text" class="form-control" id="title" name="title" value="{{old('title')}}">
+                                <input type="text" class="form-control {{$errors->has('title') ? 'border-danger' : ''}}" id="title" name="title" value="{{old('title')}}">
+                                <small class="form-text text-danger">{{$errors->first('title')}}</small>
                             </div>
+
                             <div class="form-group">
                                 <label for="image">Image</label>
-                                <input type="text" class="form-control" id="image" name="image" placeholder="https://picsum.photos/200" value="{{old('image')}}">
+                                <input type="text" class="form-control {{$errors->has('image') ? 'border-danger' : ''}}" id="image" name="image" placeholder="https://picsum.photos/200" value="{{old('image')}}">
+                                <small class="form-text text-danger">{{$errors->first('image')}}</small>
                             </div>
+
                             <div class="form-group">
                                 <label for="description">Description</label>
-                                <textarea class="form-control" id="description" name="description" rows="5">{{old('description')}}</textarea>
+                                <textarea class="form-control {{$errors->has('description') ? 'border-danger' : ''}}" id="description" name="description" rows="5">{{old('description')}}</textarea>
+                                <small class="form-text text-danger">{{$errors->first('description')}}</small>
                             </div>
+
                             <input class="btn btn-primary mt-4" type="submit" value="Save Post">
                         </form>
                         <a class="btn btn-primary float-right" href="/posts"><i class="fa-solid fa-circle-arrow-up"></i> Back</a>
