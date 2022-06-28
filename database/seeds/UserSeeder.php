2c410c6e8002d94dc0b5849bc823e6f02247c1cf
VictorEnache <victor22enache@gmail.com>
2022-06-28 19:01:06 +0200

    seed post_tag table with random number

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 class UserSeeder extends Seeder
 {
     /**
@@ -13,11 +13,26 @@ class UserSeeder extends Seeder
     {
         factory(App\User::class, 10)->create()
         ->each(function($user){
-            factory(App\Post::class, rand(1,4))->create(
+            factory(App\Post::class, rand(1,8))->create(
                 [
                     'user_id' => $user->id
                 ]
-            );
+            )
+            ->each(function($post){
+                $tag_ids = range(1,8);
+                shuffle($tag_ids);
+                $assignments =  array_slice($tag_ids, 0, rand(0,8));
+                foreach($assignments as $tag_id){
+                    DB::table('post_tag')
+                    ->insert([
+                        'post_id' => $post->id,
+                        'tag_id' => $tag_id,
+                        'created_at' => Now(),
+                        'updated_at' => Now()
+
+                    ]);
+                }
+            });
         });
     }
 }
